7d327e8716f696f6ceab2570ffb2bc2dccc364ea
tahtwitch <gravitox22@gmailcom>
2025-05-30 01:20:29 +0100

    initial commit

---
+++ src/lib/data.ts
@@ -0,0 +1,134 @@
+export const personalInfo = {
+  name: "Omowaye Oluwadamilare Kolade",
+  location: "Ikorodu, Lagos",
+  email: "tachtwitch@gmail.com",
+  github: "https://github.com/tachtwitch",
+  linkedin: "https://www.linkedin.com/in/tachtwitch-97a3b4264",
+};
+
+export const workExperience = [
+  {
+    company: "CodSoft",
+    location: "Remote",
+    position: "Android Developer Intern",
+    period: "December 2024 - January 2025",
+    achievements: [
+      "Built interactive Apps with jetpack Compose.",
+      "Implemented data storage and retrieval using SQLite and Room persistence library, achieving reduction in data retrieval time"
+    ],
+  },
+  {
+    company: "HNG TECH",
+    location: "Remote",
+    position: "Backend Intern",
+    period: "Feb 2025 - April 2025",
+    achievements: [
+      "Developed robust APIs through coding tasks, consistently meeting deadlines within 3 months, which improved my development ability and improved my overall backend skill",
+      "Engineered an AI sales agent with a team of 3 within 1 month that identified business leads using Apollo Hunter, and RapidAPI with Java Spring boot",
+      "Implemented CI/CD pipelines along with backend APIs within 3 months, enhancing deployment speed through effective team collaboration and adherence to standard git practices"
+    ],
+  },
+  {
+    company: "Telex.im",
+    location: "Remote",
+    position: "Backend Developer",
+    period: "April 2025 - May 2025",
+    achievements: [
+      "Built interactive UIs with React.js, focusing on reusable components and performance optimization.",
+      "Implemented JavaScript-based features like user authentication and real-time updates.",
+    ],
+  },
+];
+
+export const education = [
+  {
+    institution: "Bells University Of Technology",
+    location: "Ota, Ogun State",
+    degree: "B Tech Computer Science",
+    period: "Oct 2021 - Jul 2026",
+  },
+];
+export const skills = {
+  programmingLanguages: [
+    "TypeScript",
+    "JavaScript",
+    "Python",
+    "C++",
+    "Java",
+    "Rust",
+    "Kotlin"
+  ],
+  frontendDevelopment: [
+    "Nextjs",
+    "Reactjs",
+    "Tailwind CSS",
+    "HTML",
+    "CSS",
+  ],
+  mobileDevelopment:["Native Android with Jepack Compose","Compose MultiPlatform"],
+  backendDevelopment: ["Nodejs", "Expressjs", "Spring Boot"],
+  databaseAndStorage: ["PostgreSQL", "Prisma (ORM)"],
+  cloudAndDevOps: ["AWS"],
+  toolsAndServices: [
+    "Zod",
+  ],
+};
+
+export const projects = [
+  {
+    title: "Bells University Interactive Study Hub",
+    github: "https://github.com/tachtwitch/Buish",
+    duration:"Nov 2024 - Jan 2025 (will revisit)",
+    description: [
+      "Fullstack Project developed with Next.js and React.js",
+      "Leveraged YouTube API to deliver a smooth interface.",
+      "Orchestrated the development of scalable database schemas and optimized queries through Prisma ORM.",
+    ],
+  },
+  {
+    title: "Identify",
+    github: "null",
+    duration:"Nov 2024 - Present",
+    description: [
+      "An attendance app that will leveraging AI for facial authentication and goole maps for location verification.",
+      "Features will include live classes info, smartwatch integration for passkey auth and assignmment Hub.",
+      "Building with Jetpack Compose, Supabase and SpringBoot.",
+    ],
+  },
+  {
+    title: "Jetpack Task app",
+    github: "https://github.com/tachtwitch/task-app",
+    duration:"Dec 2024 - Dec 2024",
+    description: [
+    "Developed a task management app with Kotlin and Jetpack Compose.",
+    "leveraging Room persistence library for seamless data storage and retrieval.",
+    "Implemented features for efficient task organization and management."
+      ],
+    },
+    {
+      title: "Jetpack Notify App",
+      github: "https://github.com/tachtwitch/notify",
+      duration:"Dec 2024 - Dec 2024",
+      description: [
+      "Jetpack Project developed with Kotlin, jetpack compose and Room persistence library",
+      "Leveraged android notification system"
+        ],
+      },
+];
+
+export const awards = [
+  {
+    name: "Bells x Hult Prize",
+    issuer: "Hult Prize",
+    date: "Mar 25",
+    type: "Local Qualifiers",
+    position: "Fourth Place",
+  },
+  {
+    name: "HNG Finalist",
+    issuer: "HNG TECH",
+    date: "April 25",
+    type: "Certification",
+    position: "Backend Intern Finalist",
+  }
+];
